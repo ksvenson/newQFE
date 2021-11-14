df97787b54dedb4449a081952437e78a294d4af2
Evan Owen <ekowen86@gmail.com>
2021-11-14 16:22:07 -0500

    Add beta_mult and zero momentum correlator to ising_flat_crit

--- examples/ising_flat_crit.cc
+++ examples/ising_flat_crit.cc
@@ -44,12 +44,14 @@ double find_crit(double k1, double k2, double k3) {
 int main(int argc, char* argv[]) {
 
   int N = 32;
-  printf("N: %d\n", N);
 
   // choose weights for the 3 directions and calculate beta critical
   double k1 = 1.0;
   double k2 = 1.0;
   double k3 = 1.0;
+
+  // multiplier to move above or below the critical point
+  double beta_mult = 1.0;
 
   int n_therm = 2000;
   int n_traj = 50000;
@@ -62,6 +64,7 @@ int main(int argc, char* argv[]) {
     { "k1", required_argument, 0, 'a' },
     { "k2", required_argument, 0, 'b' },
     { "k3", required_argument, 0, 'c' },
+    { "beta_mult", required_argument, 0, 'm' },
     { "n_therm", required_argument, 0, 'h' },
     { "n_traj", required_argument, 0, 't' },
     { "n_skip", required_argument, 0, 's' },
@@ -70,7 +73,7 @@ int main(int argc, char* argv[]) {
     { 0, 0, 0, 0 }
   };
 
-  const char* short_options = "N:a:b:c:h:t:s:w:e:";
+  const char* short_options = "N:a:b:c:m:h:t:s:w:e:";
 
   while (true) {
 
@@ -83,6 +86,7 @@ int main(int argc, char* argv[]) {
       case 'a': k1 = std::stod(optarg); break;
       case 'b': k2 = std::stod(optarg); break;
       case 'c': k3 = std::stod(optarg); break;
+      case 'm': beta_mult = std::stod(optarg); break;
       case 'h': n_therm = atoi(optarg); break;
       case 't': n_traj = atoi(optarg); break;
       case 's': n_skip = atoi(optarg); break;
@@ -96,11 +100,12 @@ int main(int argc, char* argv[]) {
   printf("k1: %.12f\n", k1);
   printf("k2: %.12f\n", k2);
   printf("k3: %.12f\n", k3);
+  printf("beta_mult: %.12f\n", beta_mult);
 
   QfeLattice lattice;
   lattice.InitTriangle(N, k1, k2, k3);
 
-  QfeIsing field(&lattice, find_crit(k1, k2, k3));
+  QfeIsing field(&lattice, find_crit(k1, k2, k3) * beta_mult);
   field.HotStart();
 
   printf("beta: %.12f\n", field.beta);
@@ -113,6 +118,11 @@ int main(int argc, char* argv[]) {
   std::vector<QfeMeasReal> corr_w(N/2);
   std::vector<QfeMeasReal> corr_xz(N/2);
   std::vector<QfeMeasReal> corr_yz(N/2);
+
+  // correlator averaged over zero momentum mode
+  std::vector<QfeMeasReal> corr_zero_x(N/2);
+  std::vector<QfeMeasReal> corr_zero_y(N/2);
+  std::vector<QfeMeasReal> corr_zero_z(N/2);
 
   // measurements
   std::vector<double> mag;
@@ -142,6 +152,9 @@ int main(int argc, char* argv[]) {
     std::vector<int> corr_w_sum(N, 0);
     std::vector<int> corr_xz_sum(N, 0);
     std::vector<int> corr_yz_sum(N, 0);
+    std::vector<int> corr_zero_x_sum(N, 0);
+    std::vector<int> corr_zero_y_sum(N, 0);
+    std::vector<int> corr_zero_z_sum(N, 0);
     int count = field.wolff_cluster.size();
     for (int i1 = 0; i1 < count; i1++) {
       for (int i2 = i1; i2 < count; i2++) {
@@ -164,6 +177,7 @@ int main(int argc, char* argv[]) {
 
         int dx = (N - abs(2 * abs(x1 - x2) - N)) / 2;
         int dy = (N - abs(2 * abs(y1 - y2) - N)) / 2;
+        int dw = (N - abs(2 * abs(w1 - w2) - N)) / 2;
 
         if (y1 == y2) corr_x_sum[dx]++;
         if (x1 == x2) corr_y_sum[dy]++;
@@ -171,6 +185,9 @@ int main(int argc, char* argv[]) {
         if (z1 == z2) corr_w_sum[dx]++;
         if (xz1 == xz2) corr_xz_sum[dy]++;
         if (yz1 == yz2) corr_yz_sum[dx]++;
+        corr_zero_x_sum[dy]++;
+        corr_zero_y_sum[dx]++;
+        corr_zero_z_sum[dw]++;
       }
     }
 
@@ -182,6 +199,9 @@ int main(int argc, char* argv[]) {
       corr_w[i].Measure(double(corr_w_sum[i]) / double(count));
       corr_xz[i].Measure(double(corr_xz_sum[i]) / double(count));
       corr_yz[i].Measure(double(corr_yz_sum[i]) / double(count));
+      corr_zero_x[i].Measure(double(corr_zero_x_sum[i]) / double(count * N));
+      corr_zero_y[i].Measure(double(corr_zero_y_sum[i]) / double(count * N));
+      corr_zero_z[i].Measure(double(corr_zero_z_sum[i]) / double(count * N));
     }
 
     action.push_back(field.Action());
@@ -257,6 +277,24 @@ int main(int argc, char* argv[]) {
   for (int i = 0; i < N/2; i++) {
     printf("5 %04d %.12e %.12e\n", i, corr_yz[i].Mean(), corr_yz[i].Error());
     fprintf(file, "5 %04d %.12e %.12e\n", i, corr_yz[i].Mean(), corr_yz[i].Error());
+  }
+
+  printf("\ncorr_zero_x:\n");
+  for (int i = 0; i < N/2; i++) {
+    printf("6 %04d %.12e %.12e\n", i, corr_zero_x[i].Mean(), corr_zero_x[i].Error());
+    fprintf(file, "6 %04d %.12e %.12e\n", i, corr_zero_x[i].Mean(), corr_zero_x[i].Error());
+  }
+
+  printf("\ncorr_zero_y:\n");
+  for (int i = 0; i < N/2; i++) {
+    printf("7 %04d %.12e %.12e\n", i, corr_zero_y[i].Mean(), corr_zero_y[i].Error());
+    fprintf(file, "7 %04d %.12e %.12e\n", i, corr_zero_y[i].Mean(), corr_zero_y[i].Error());
+  }
+
+  printf("\ncorr_zero_z:\n");
+  for (int i = 0; i < N/2; i++) {
+    printf("8 %04d %.12e %.12e\n", i, corr_zero_z[i].Mean(), corr_zero_z[i].Error());
+    fprintf(file, "8 %04d %.12e %.12e\n", i, corr_zero_z[i].Mean(), corr_zero_z[i].Error());
   }
 
   return 0;

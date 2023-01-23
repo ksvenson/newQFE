b511a89efd69c8173f5900596137640ec499ec1a
ekowen86 <ekowen86@gmail.com>
2023-01-23 16:21:42 -0500

    Rename lattice_test to phi4_test

--- tests/phi4_test.cc
+++ tests/phi4_test.cc
@@ -8,15 +8,14 @@
 #include "statistics.h"
 
 // phi4 theory on a square lattice has a critical point near lambda = 0.25,
-// msq = -1.27 [1]. On a 64^2 lattice it takes about 4 wolff cluster sweeps to
-// get a net cluster size roughly equal to the entire lattice. After the cluster
-// update, we perform an metropolis sweep and then a overrelaxation sweep. We
-// take measurements every 20 iterations, which leads to reasonable
-// autocorrelation times for the 2nd and 4th magnetic moments. With these
-// parameters, we expect the 4th order binder cumulant to be near its critical
-// value of 0.8. we also expect to see a peak in the magnetic susceptibility,
-// which can be seen by varying msq. We can also check that the overralation
-// demon is close to 1.
+// msq = -1.27251 [1]. We perform a combination of Wolff cluster updates,
+// Metropolis updates, overrelaxation updates and Swendsen-Wang updates such
+// that we obtain an independent sample every 20 sweeps. We take measurements
+// every 20 iterations, which leads to reasonable autocorrelation times for the
+// 2nd and 4th magnetic moments. With these parameters, we expect the 4th order
+// binder cumulant to be near its critical value of ~0.9. we also expect to see
+// a peak in the magnetic susceptibility, which can be seen by varying msq. We
+// can also check that the overralation demon is close to 1.
 
 // [1] D. Schaich, W. Loinaz, Phys. Rev. D 79, 056008 (2009).
 
@@ -24,8 +23,8 @@ int main(int argc, char* argv[]) {
   int N = 64;
   printf("N: %d\n", N);
 
-  double msq = -1.27;
-  printf("msq: %.4f\n", msq);
+  double msq = -1.27251;
+  printf("msq: %.6f\n", msq);
 
   double lambda = 0.25;
   printf("lambda: %.4f\n", lambda);
@@ -36,6 +35,7 @@ int main(int argc, char* argv[]) {
 
   QfePhi4 field(&lattice, msq, lambda);
   field.HotStart();
+  field.metropolis_z = 2.0;
 
   printf("initial action: %.12f\n", field.Action());
 
@@ -52,18 +52,24 @@ int main(int argc, char* argv[]) {
   int n_therm = 5000;
   int n_traj = 20000;
   int n_skip = 20;
-  int n_wolff = 4;
+  int n_metropolis = 8;
+  int n_wolff = 5;
   for (int n = 0; n < (n_traj + n_therm); n++) {
     int cluster_size_sum = 0;
     for (int j = 0; j < n_wolff; j++) {
       cluster_size_sum += field.WolffUpdate();
     }
+    double metropolis_sum = 0.0;
+    for (int j = 0; j < n_metropolis; j++) {
+      metropolis_sum += field.Metropolis();
+    }
     cluster_size.Measure(double(cluster_size_sum) / vol);
-    accept_metropolis.Measure(field.Metropolis());
+    accept_metropolis.Measure(metropolis_sum);
     accept_overrelax.Measure(field.Overrelax());
     demon.Measure(field.overrelax_demon);
 
     if (n % n_skip || n < n_therm) continue;
+    field.SWUpdate();
 
     action.Measure(field.Action());
     double m = field.MeanPhi();

3b3b0ebd252dfd41b497dcf9ba37408ad6a36979
ksvenson <kai.svenson628@gmail.com>
2024-08-08 20:27:24 +0100

    Moved comparison of algorithms to separate file

--- sweeps/param_sweep.py
+++ sweeps/param_sweep.py
@@ -225,53 +225,6 @@ class Sweep():
         self.create()
 
 
-def comp_sweeps(wolff, sw, config_idx, free_idx, dir):
-    assert not wolff.sw
-    assert sw.sw
-
-    res_wolff = wolff.read_results()
-    if res_wolff is None:
-        print('Can not compare sweeps. Missing data in wolff sweep.')
-        return
-    avg_wolff, var_wolff = res_wolff
-
-    res_sw = sw.read_results()
-    if res_sw is None:
-        print('Can not compare sweeps. Missing data in sw sweep.')
-        return
-    avg_sw, var_sw = res_sw
-
-    plot_idx = list(config_idx) + [slice(None)]
-    plot_idx[free_idx] = slice(None)
-    plot_idx = tuple(plot_idx)
-
-    avg = avg_wolff[plot_idx] - avg_sw[plot_idx]
-    var = var_wolff[plot_idx]/wolff.ntraj + var_sw[plot_idx]/sw.ntraj
-    sig = avg / np.sqrt(var)
-
-    beta = wolff.beta[plot_idx]
-    beta_union = np.array([])
-    for beta_space in beta:
-        beta_union = np.union1d(beta_union, beta_space)        
-    k_space = wolff.k[free_idx]
-
-    plot_sig = np.full((sig.shape[0], len(beta_union), sig.shape[-1]), np.nan)
-    for k_idx, k_row in enumerate(plot_sig):
-        k_row[np.isin(beta_union, beta[k_idx])] = sig[k_idx, :]
-
-    ylabel = rf'$k_{free_idx}$'
-    for stat_idx, stat in enumerate(Sweep.headers):
-        if not stat.plot:
-            continue
-
-        fig, ax = plt.subplots()
-        pcm = ax.pcolormesh(beta_union, k_space, plot_sig[..., stat_idx], shading='nearest')
-        fig.colorbar(pcm)
-        ax.set_xlabel(r'$\beta$')
-        ax.set_ylabel(ylabel)
-        ax.set_title(f'{stat.axis}')
-        fig.savefig(f'{dir}/{stat.label}.svg', **FIG_SAVE_OPTIONS)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--base', required=True)
@@ -287,7 +240,6 @@ if __name__ == '__main__':
     parser.add_argument('--refine-nwolff', action='store_true')
     parser.add_argument('--refine-beta', action='store_true')
     parser.add_argument('--edit', action='store_true')
-    parser.add_argument('--compare', nargs=3, default=None)
     args = parser.parse_args()
 
     if args.base.endswith('/'):
@@ -336,11 +288,3 @@ if __name__ == '__main__':
             sweep.nodes = args.nodes
             sweep.ntraj = args.ntraj
             sweep.create()
-    
-    if args.compare is not None:
-        for idx in range(len(args.compare)):
-            if args.compare[idx].endswith('/'):
-                args.compare[idx] = args.compare[idx][-1]
-        wolff = Sweep.load(args.compare[0])
-        sw = Sweep.load(args.compare[1])
-        comp_sweeps(wolff, sw, (0,)*13, FCC_IDX[-1], args.compare[2])
